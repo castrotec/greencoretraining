ff75081471908c32dde42ed61c3bfcca1dda801e
Jorge Castro <castrotec@gmail.com>
2018-11-05 18:48:26 -0600

    Objetos

---
+++ semana8/objetos.py
@@ -0,0 +1,11 @@
+
+#Notes
+'''
+_ un underscore: privado
+__ dos underscore: protected
+ningun un#Herencia siempre implica polimorfismoderscore: publico
+
+#Herencia siempre implica polimorfismo
+'''
+
+#
